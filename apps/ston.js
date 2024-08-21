3529d9f56fcb3bade60e32d1d9fa6344bfcfacd0
excellen114514 <xiuxianing@foxmail.com>
2024-08-21 20:08:36 +0800

    更新三个功能的注释和优化自己的代码风格（）

--- apps/ston.js
+++ apps/ston.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 const Steam = /^(#|\/)?steam在线$/
 const count = 10 // 返回排行榜数量，推荐不要太高，可能会导致无法发送
-export class example extends plugin {
+export class steam extends plugin {
     constructor() {
         super({
             name: 'steamonline',
@@ -20,17 +20,17 @@ export class example extends plugin {
 
     async Ston(e) {
         const FETCH_DATA = await fetch(`http://api.yujn.cn/api/steam.php?type=json&count=${count}`);
-        const DATA_JSON = await FETCH_DATA.json();
-        const MESSAGE = ['Steam游戏在线人数']
+        const DATA_JSON = await FETCH_DATA.json();//获取json字符串并解析
+        const msg = ['Steam游戏在线人数']
         for (let i = 0; i < count; i++) {
             const DATA = DATA_JSON['data'][i]
             const MSG = [
-                '\n排名：' + DATA['index'],
-                '\n游戏名称：', DATA['name'],
-                '\n在线人数：' + DATA['current']
+                '游戏排名\n：' + DATA['index'],
+                '游戏名称\n：', DATA['name'],
+                '在线人数\n：' + DATA['current']
             ]
-            MESSAGE.push(MSG)
+            msg.push(MSG)
         }
-        return e.reply(MESSAGE)
+        return e.reply(msg)
     }
 }
--- apps/独立小插件.js
+++ apps/独立小插件.js
@@ -1,11 +1,11 @@
-
+﻿
  import fetch from "node-fetch";
  import fs from 'fs';
 
-const C_REGEX = /^(#|\/)?涩图$/
-const D_REGEX = /^(#|\/)?ehelp$/
-const S_REGEX = /^(#|\/)?祖安一句$/
-const G_REGEX = /^(#|\/)?来点视频$/
+const Setu = /^(#|\/)?涩图$/
+const Ehelp = /^(#|\/)?ehelp$/
+const Zuan = /^(#|\/)?祖安一句$/
+const Comess = /^(#|\/)?来点视频$/
 
 export class excellen extends plugin {
     constructor() {
@@ -15,16 +15,16 @@ export class excellen extends plugin {
             event: 'message',
             priority: 1,
             rule: [{
-                    reg: C_REGEX,
+                    reg: Setu,
                     fnc: "Setu"
                 }, {
-                    reg: D_REGEX,
+                    reg: Ehelp,
                     fnc: "Ehelp"
                 }, {
-                    reg: S_REGEX,
+                    reg: Zuan,
                     fnc: "Zuan"
                 }, {
-                    reg: G_REGEX,
+                    reg: Comess,
                     fnc: "Comess"
                 },
             ]
@@ -32,71 +32,79 @@ export class excellen extends plugin {
         )
     }
 
-    async Setu(e) {
-        
-        
-        //ִ收到命令并执行
+    async Setu(e){
+        //ִ收到命令后执行
+
         let url = 'https://image.anosu.top/pixiv/json?r18=1';
         let res = await fetch(url).catch((err) => logger.error(err))
-        //判定接口是否可用
+
+        //处理url获取失败
         if (!res) {
             logger.error('[setu] 接口请求失败')
             return await this.reply('错误，请反馈！')
         }
-
+        //这里是text，意味着json字符串同样文本发送
         res = await res.text()
-        //输出到控制台
+        //输出结果到控制台
         logger.info(`[接口结果] setu：${res}`)
 
-        //返回结果 
+        //回复
         await this.reply(`${res}`)
+    
     }
 
     async Zuan(e) {
-        //收到命令并执行
+        //收到命令后执行
         let url = 'https://act.jiawei.xin:10086/lib/api/maren.php?catalog=yang'
         let res = await fetch(url).catch((err) => logger.error(err))
-        //判定接口是否可用
+
+        //处理url获取失败
         if (!res) {
             logger.error('[zuan] 接口请求失败')
             return await this.reply('错误，请反馈！')
         }
-
+        //这里是text，意味着json字符串同样文本发送
         res = await res.text()
-        //输出到控制台
+        //输出结果到控制台
         logger.info(`[接口结果] zuan：${res}`)
 
-        //返回结果 
+        //回复
         await this.reply(`${res}`)
     }
 
     async Comess(e) {
-        
-        //ִ收到命令并执行
+        //收到消息后处理
         let url = "http://api.yujn.cn/api/juhexjj.php?type=video";
         let res = await fetch(url).catch((err) => logger.error(err));
-        let msg = [segment.at(e.user_id), segment.video(res.url)];
 
+         //处理url获取失败
+         if (!res) {
+            logger.error('[comess] 接口请求失败')
+            return await this.reply('错误，请反馈！')
+        }
+        let msg = [segment.at(e.user_id), segment.video(res.url)];//由于url的获取结果是video，所以直接写输出video不解析了
+        //输出结果到控制台
+        logger.info(`[接口结果] video处理完成`)
         //回复
         e.reply(msg);
 
-        return true; //返回 true 阻挡消息不再往下
+        return true; //返回true，阻挡消息不再往下
 
     }
 
     async Ehelp(e) {
         {
-            const path = process.cwd() + '/plugins/ExceSama-plugin/resources/help';
-            const files = fs.readdirSync(path);
-            const imageFiles = files.filter(file => file.endsWith('.jpg') || file.endsWith('.png'));
-            const randomIndex = Math.floor(Math.random() * imageFiles.length);
-            const imagePath = `${path}/${imageFiles[randomIndex]}`;
-            const img = fs.readFileSync(imagePath);
-            await this.reply(segment.image(img));
+            const path = process.cwd() + '/plugins/ExceSama-plugin/resources/help';//设立一个文件夹并从中提出图片
+            const files = fs.readdirSync(path);//调用fs模块
+            const imgFiles = files.filter(file => file.endsWith('.jpg') || file.endsWith('.png'));//可以再扩写其他格式
+            const randomIndex = Math.floor(Math.random() * imgFiles.length);
+            const imgPath = `${path}/${imgFiles[randomIndex]}`;
+            const img = fs.readFileSync(imgPath);
+             await this.reply(segment.image(img));//输出文件夹图片
         }
     }
 
-
+    
 
 }
 
